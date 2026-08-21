8a083577dda3ddb1b74d9f4d76e4119031e87433
Idan Koblik <me@idank.dev>
2026-08-21 17:35:24 +0300

    feat: Add image raw inspection cmd

--- include/codecs/lsb.h
+++ include/codecs/lsb.h
@@ -3,6 +3,8 @@
 
 #include "codec.h"
 
+#define LSB_FILTER 0XFE
+
 extern const Codec LsbCodec;
 
 #endif // LSB_H_
---
+++ src/cmd/inspect.c
@@ -0,0 +1,81 @@
+#include "cmd/command.h"
+#include "log.h"
+#include "stb.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+int exec(int argc, char *argv[]) {
+    if (argc < 1)
+        return EXEC_USAGE_ERROR;
+
+    const char *target = argv[0];
+    size_t line_limit = 1;
+
+    if (argc == 3) {
+        const char *flag = argv[1];
+        if (strcmp(flag, "-l") != 0)
+            return EXEC_GENERIC_ERROR;
+
+        const char *flag_value = argv[2];
+        line_limit = atoi(flag_value);
+        if (line_limit <= 0) {
+            ERROR("Line limit cannot be equal or less then 0");
+            return EXEC_GENERIC_ERROR;
+        }
+    }
+
+    int width, height, channels;
+    unsigned char *pixels = stbi_load(
+        target,
+        &width,
+        &height,
+        &channels,
+        0 // Any
+    );
+
+    if (!pixels) {
+        ERROR("Failed to load the image (%s)", target);
+        return -1;
+    }
+
+    size_t pixels_len = (size_t)width * height * channels * sizeof(*pixels);
+    INFO("Image: %dx%d, %d channels", width, height, channels);
+    size_t lines = 0;
+
+    for (size_t i = 0; i + 7 < pixels_len; i += 8) {
+        unsigned char byte = 0;
+
+        for (size_t j = 0; j < 8; j++)
+            byte |= ((pixels[i + j] & 1) << j);
+
+        char line[64];
+        int len = 0;
+
+        // Binary
+        for (int bit = 7; bit >= 0; bit--)
+            len += snprintf(line + len, sizeof(line) - len, "%d", (byte >> bit) & 1);
+
+        // Hex
+        len += snprintf(line + len, sizeof(line) - len, "  0x%02x  ", byte);
+
+        // ASCII
+        len += snprintf(line + len, sizeof(line) - len, "%c\n", (byte >= 32 && byte <= 126) ? byte : '.');
+
+        if (isatty(STDOUT_FILENO)) {
+            if (lines < line_limit) {
+                fputs(line, stdout);
+                lines++;
+            }
+        } else fputs(line, stdout);
+    }
+
+    INFO("To see the rest of the inspect pipe the command to a text file.");
+    return EXEC_OK;
+}
+
+static const struct Command inspect_cmd = {.name = "inspect",
+    .description = "Inspect data of an image",
+    .usage = "inspect <target_file> [-l <line_limit>]\n",
+    .exec = exec};
+
+COMMAND(inspect_cmd);
